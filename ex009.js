4e629ed17280e1dd8dd8fc4bb752ba694b6838a2
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-05-04 15:24:27 -0300

    exs atualizados

---
+++ ex009.js
@@ -0,0 +1,7 @@
+var  vel = 78.5 
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+console.log('Dirija sempre usando o cinto de segurança')
+//if = se 
+if (vel > 60) { //condição simples! 
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
