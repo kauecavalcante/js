24e5ee7cb5d9995d2c2d6bdb111158c683cc0c69
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-05-06 16:20:35 -0300

    novos exs

--- ex016/main.js
+++ ex016/main.js
@@ -1,1 +1,27 @@
-window.alert('ola')
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+//var hora = 19
+msg.innerHTML= `Agora são ${hora} horas.`
+
+if (hora >= 0 && hora <12) {
+    //bom dia
+    img.src = 'manhã.png'
+    document.body.style.background = '#e2cd9f'
+} else if (hora >= 12 && hora < 18) {
+    //boa tarde
+    img.src = 'tarde.jpg'
+    document.body.style.background = '#b9846f'
+} else {
+    // boa noite
+    img.src = 'noite.jpg'
+    document.body.style.background = '#515154'
+}
+
+
+
+
+}
+
