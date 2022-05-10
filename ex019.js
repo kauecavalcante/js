ebb53435637f6fd59aef59d574e9f2a76801d523
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-05-10 14:44:33 -0300

    Create ex019.js

---
+++ ex019.js
@@ -0,0 +1,20 @@
+//Repetições 
+
+// for (inicio; teste; incr) {   
+
+  // }
+
+console.log('Vai começar...')
+for (var c=1; c<=4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+// mesma coisa do que 
+/* 
+var c = 1 
+while ( c <= 11) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
