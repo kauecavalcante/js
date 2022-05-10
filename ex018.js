dc9478f95c9a549be1f368be20c6b9198548384b
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-05-10 14:22:37 -0300

    Create ex018.js

---
+++ ex018.js
@@ -0,0 +1,17 @@
+//repetições 
+
+var c = 1
+do { // do = faça
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6) { //while = enquanto
+   console.log(`Passo ${c}`)
+   c++ //c = c + 1 
+
+} */
+
+ 
